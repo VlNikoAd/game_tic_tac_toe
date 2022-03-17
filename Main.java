798148d5d67dd81e958d0417766f18152b62582e
VlNikoAd <nikolaenko_vladislav@icloud.com>
2022-03-17 20:54:02 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,77 @@
+package game_tic_tac_toe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static final int SIZE = 3;
+    public static final char EMPTY = '-';
+    public static final char CROSS = 'x';
+    public static final char ZERO = '0';
+
+    public static void main(String[] args) {
+        char[][] field = new char[SIZE][SIZE];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                field[i][j] = EMPTY;
+            }
+        }
+        Scanner scanner = new Scanner(System.in);
+        boolean isCrossTurn = true;
+
+        while (true) {
+            System.out.println("Ходят " + (isCrossTurn ? "крестики" : "нолики") + "!");
+            printField(field);
+            String input = scanner.nextLine(); // 1 2
+            String[] parts = input.split(" ");
+            int x = Integer.parseInt(parts[0]) - 1;
+            int y = Integer.parseInt(parts[1]) - 1;
+
+            if (field[x][y] != EMPTY) {
+                continue;
+            }
+            field[x][y] = isCrossTurn ? CROSS : ZERO;
+
+            if (isWin(field, isCrossTurn ? CROSS : ZERO)) {
+                System.out.println("выйграли " + (isCrossTurn ? "крестики" : "нолики") + "!");
+                printField(field);
+                break;
+            } else {
+                if (isCrossTurn) {
+                    isCrossTurn = false;
+                } else {
+                    isCrossTurn = true;
+                }
+            }
+        }
+    }
+    public static boolean isWin(char[][] field, char player) {
+        if (field[0][0] == player && field[0][1] == player && field[0][2] == player )
+            return true;
+        if (field[1][0] == player && field[1][1] == player && field[1][2] == player )
+            return true;
+        if (field[2][0] == player && field[2][1] == player && field[2][2] == player )
+            return true;
+
+        if (field[0][0] == player && field[1][0] == player && field[2][0] == player )
+            return true;
+        if (field[0][1] == player && field[1][1] == player && field[2][1] == player )
+            return true;
+        if (field[0][2] == player && field[1][2] == player && field[2][2] == player )
+            return true;
+
+        if (field[0][0] == player && field[1][1] == player && field[2][2] == player )
+            return true;
+        if (field[2][0] == player && field[1][1] == player && field[0][2] == player )
+            return true;
+
+        return false;
+    }
+    public static void printField (char[][] field) {
+        for (char[] row : field) {
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
